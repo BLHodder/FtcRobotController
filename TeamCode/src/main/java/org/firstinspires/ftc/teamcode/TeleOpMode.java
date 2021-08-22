defc9c3e2747b887f6e4b4c48fff11da8e7be5c0
BLHodder <61534159+BLHodder@users.noreply.github.com>
2021-08-22 19:24:37 +1000

    setup of teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class TeleOpMode extends OpMode {
+
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void init_loop() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+}
